0095bfa3ef9fdf99f9d55ae8d58879a163397445
katherine tuter <katherinetuter@gmail.com>
2017-02-25 09:32:20 -0600

    deleted extra song array code

--- songs.js
+++ songs.js
@@ -6,14 +6,9 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-function addMusic() {
-}
-
-var allSongs = songs.push(songs[songs.length]);
 console.log(songs);
 
 //remove typos
-console.log(typeof songs);
 var strSong = songs.toString();
 var typos = [];
 typos = strSong.replace(/[*@(!)]/g, "");
